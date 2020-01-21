49c8e243f2b05b7a213a3746d6719f2b43701396
Alina Shilo <alinashilo98@gmail.com>
2020-01-21 16:02:02 +0300

    hometask-20.01.20

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 let temp = 'Это строка';
 alert (temp);
-temp = temp.replace('Это', 'И это тоже');
+temp = temp.replace(temp, 'И тут тоже строка');
 console.log(temp);
+
+/////////////////////////////////////////////////////////////
+
+let user = {
+    name: 'Alina',
+    years: 21,
+    women: true,
+    id: null,
+    interests: ['cycling', 'photography']
+};
+
+for (let key in user) {
+    alert(key + ': ' + user[key]);
+}
+console.table(user);
+
+/////////////////////////////////////////////////////////////
+
+let age = prompt('Сколько Вам лет?');
+alert(`Поздравляем, Вам ${age} полных лет!`);
+if (age >= 18) {
+    alert('Пользователь совершеннолетний.');
+} else {
+    alert('Пользователь несовершеннолетний.');
+}
