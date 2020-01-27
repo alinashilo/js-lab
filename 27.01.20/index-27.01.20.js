866118c7acc01939cfbb81bd72bd503f6030a417
Alina Shilo <alinashilo98@gmail.com>
2020-01-27 14:17:48 +0300

    hometask-27.01.20

---
+++ 27.01.20/index-27.01.20.js
@@ -0,0 +1,40 @@
+function isPalindrome() {
+    let str = prompt('Введите слово для проверки на палиндромность:');
+    str = str.toLowerCase();
+    if(!str) {
+        console.log('Введите слово!');
+    } else if (str === str.split('').reverse().join('')) {
+        console.log('Слово является палиндромом');
+    } else {
+        console.log('Слово не является палиндромом');
+    }
+}
+
+isPalindrome();
+
+/////////////////////////////////////////////////////////////////////////////////
+
+function uniqueArray() {
+    let arr = [];
+    arr.length = 7;
+    let item;
+    for (let i = 0; i < arr.length; i++) {
+        item = Math.floor(Math.random() * 11);
+        if (!arr.includes(item)) {
+            arr[i] = item;
+        } else i--;
+    }
+    console.log(arr);
+    console.log(arr.sort(function (a, b) {
+        if (a < b) {
+            return 1;
+        }
+        if (a > b) {
+            return -1;
+        }
+    }));
+}
+
+uniqueArray();
+
+/////////////////////////////////////////////////////////////////////////////////
