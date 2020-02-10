f5e1444bf7f366223623df50e7413e4f0a0e2d1f
Alina Shilo <alinashilo98@gmail.com>
2020-02-10 15:00:37 +0300

    commit 10/02

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,1 +1,31 @@
+function generateFizzBuzz() {
+    let n = Number(prompt('Enter the number:', ''));
+    if (!n) {
+        alert('The value is not valid. Please, enter the number');
+    } else {
+        let arr = [], arr2 = [];
+        for (let i = 1; i <= n; i++) {
+            arr.push(i);
+        }
 
+        arr2 = arr.reduce((acc, curr) => {
+            if (curr % 15 === 0) {
+                arr2.push(acc = 'fizzbuzz');
+            } else if (curr % 3 === 0) {
+                arr2.push(acc = 'fizz');
+            } else if (curr % 5 === 0) {
+                arr2.push(acc = 'buzz');
+            } else {
+                arr2.push(acc = curr);
+            }
+            return arr2;
+        }, []);
+
+        let html = '<p>Your list: </p><ul>';
+        for (let i = 0; i < n; i++) {
+            html += '<li>' + arr2[i] + '</li>';
+        }
+        html += '</ul>';
+        document.getElementById('result').innerHTML = html;
+    }
+}
